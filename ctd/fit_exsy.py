e8e08d2ea33ddaff250bc53939794faf1589f3bc
Darian T. Yang <dty7@pitt.edu>
2023-01-05 13:49:13 -0500

    was able to fit the preliminary exsy data to a curve and get the rates

--- ctd/fit_exsy.py
+++ ctd/fit_exsy.py
@@ -8,4 +8,57 @@ https://pubs.acs.org/doi/abs/10.1021/acs.jpclett.9b00052
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize
 
+plt.style.use("/Users/darian/github/wedap/wedap/styles/default.mplstyle")
+
+# import intensity ratio dataset (I_12 / I_11)
+ratios = np.loadtxt("iratios_4F.txt")
+# mixing times (skipping 600ms for now)
+times = np.array([2, 5, 10, 15, 25, 35, 50, 75, 100, 200])
+# convert ms to seconds
+times = times / 1000
+
+fig, ax = plt.subplots()
+ax.scatter(times, ratios)
+ax.set(xlabel="Time(s)", ylabel="I$_{12}$/I$_{11}$", title="4F-CA-CTD 1.2mM")
+
+def calc_iratio(t_m, k_12, k_21):
+    """
+    Calculates (I_12 / I_11) as a function of mixing time.
+    """
+    return ((1 - np.exp(-(k_12 + k_21) * t_m)) * k_12) / \
+        (k_21 + k_12 * np.exp(-(k_12 + k_21) * t_m))
+
+#print(calc_iratio(0.2, 74, 114))
+
+def calc_rates():
+    """
+    Using intensity ratios per mixing time, use lls fitting from
+    scipy.curve_fit to optimize rate constants in I(tm).
+    """
+    # fit to estimate k_12 and k_21, using 1 as initial guess
+    param, param_cov = scipy.optimize.curve_fit(calc_iratio, times, ratios)
+    print("k_12 and k_21:", param)
+
+    return param
+
+rates = calc_rates()
+Kd = rates[0] / rates[1]
+print(f"\t\tKd = {Kd}")
+
+def plot_fitted_curve(ax):
+    """
+    After curve fitting, plot I(tm) function using the
+    fitted rate constants.
+    """
+    # cover many mixing times from 0-250ms
+    x = np.linspace(0, 0.250, 500)
+    y = np.array([calc_iratio(i, rates[0], rates[1]) for i in x])
+
+    ax.plot(x, y, linestyle="--")
+
+plot_fitted_curve(ax)
+
+fig.tight_layout()
+plt.show()
