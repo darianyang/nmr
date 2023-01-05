b77e19a7783f30922f0598f145aeee88b1a624cd
Darian T. Yang <dty7@pitt.edu>
2023-01-05 14:54:39 -0500

    plotted 512NS 4F-CTD 1D and extracted peak intensities of D1 and D2 to get population-based Kd

--- ctd/1d_nmr_plot.py
+++ ctd/1d_nmr_plot.py
@@ -33,6 +33,14 @@ def plot_1d(path, ax=None, label=None, color="magenta", scale=None, xlim=(-120, 
     # read in the data from a NMRPipe file
     dic,data = ng.pipe.read(path)
 
+    # print intensity of d1 and d2 peaks
+    peaks = ng.peakpick.pick(data, 20000000)
+    p1 = peaks[0][0]
+    p2 = peaks[1][0]
+    print("Peak Intensities:", data[int(p1)], data[int(p2)])
+    # Kd = [D2] / [D1]
+    print("\t\t  Kd = ", data[int(p2)] / data[int(p1)])
+
     # optionally scale data for different concentrations
     if scale:
         data *= scale
@@ -61,10 +69,13 @@ def plot_1d(path, ax=None, label=None, color="magenta", scale=None, xlim=(-120, 
 fig, ax = plt.subplots()
 
 #plot_1d("600-2/DTY-CaCTD-F-12152022/1/test.DAT", ax=ax, label="7F", color="black")
-plot_1d("600-2/DTY-CaCTD-F-12152022/2/test.DAT", ax=ax, label="4F 1200µM", color="magenta")
-plot_1d("600-2/DTY-CaCTD-F-12152022/3/test.DAT", ax=ax, label="4F 120µM", color="k", scale=10, xlim=(-117, -135))
-ax.invert_xaxis()
-plt.legend(prop={'size': 12})
+# plot_1d("600-2/DTY-CaCTD-F-12152022/2/test.DAT", ax=ax, label="4F 1200µM", color="magenta")
+# plot_1d("600-2/DTY-CaCTD-F-12152022/3/test.DAT", ax=ax, label="4F 120µM", color="k", scale=10, xlim=(-117, -135))
+# ax.invert_xaxis()
+# plt.legend(prop={'size': 12})
+
+plot_1d("600-2/DTY-CaCTD-F-12152022/10/test.DAT", ax=ax, label="4F 512NS", xlim=(-117, -135))
+
 fig.tight_layout()
 plt.show()
-fig.savefig("figures/1d_ctd_4F_comp.png", dpi=300, transparent=True)
+#fig.savefig("figures/1d_ctd_4F_512NS.png", dpi=300, transparent=True)
