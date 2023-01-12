9f45ef00a7c9ec0cbc53baddfdc7b9ca16f8fdf4
Darian T. Yang <dty7@pitt.edu>
2023-01-12 13:03:36 -0500

    updated 4f exsy proc with -lp and -zf 4

--- ctd/600-2/DTY-CaCTD-4F-EXSY-12162022/multi_anal.sh
+++ ctd/600-2/DTY-CaCTD-4F-EXSY-12162022/multi_anal.sh
@@ -47,7 +47,8 @@ nmrPipe   -in  test.fid  \
 | nmrPipe -fn TP \
 | nmrPipe -fn SP -off 0.15 -end 0.99 -pow 1 -c 0.5 \
 | nmrPipe -fn EM -lb 40 \
-| nmrPipe -fn ZF -auto \
+| nmrPipe -fn LP \
+| nmrPipe -fn ZF -zf 4 \
 | nmrPipe -fn FT -alt \
 | nmrPipe -fn PS -p0 -32 -p1 55 -verb -di \
 | nmrPipe -fn TP \
